067cf31b1d41320c0854e05cdc282f427d975e78
r1gabriela <andradegabriela20@gmail.com>
2020-09-03 12:06:14 -0300

    server

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const port = 3000;
-const app = express();
-const cors = require('cors');
+const express = require('express')
+const app = express()
 
-app.use(cors);
+ 
 app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
-app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
 
-require('../src/routes')(app);
